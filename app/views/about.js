b446fa0f906b6754f9fd9bf339312364e252a476
Arthur Kusumadjaja <arthur.kusumadjaja@tabcorp.com.au>
2017-02-13 11:22:55 +1100

    updating content and packages

--- app/views/about.js
+++ app/views/about.js
@@ -9,39 +9,47 @@ angular.module('aku.about', [])
         $scope.skills = [
             {
                 name: 'HTML',
-                level: [1,2,3,4,5]
+                level: [1, 2, 3, 4, 5]
             },
             {
                 name: 'JS',
-                level: [1,2,3,4,5]
+                level: [1, 2, 3, 4, 5]
             },
             {
                 name: 'CSS',
-                level: [1,2,3,4]
+                level: [1, 2, 3, 4]
             },
             {
                 name: 'Ember JS',
-                level: [1,2]
+                level: [1, 2]
             },
             {
                 name: 'Angular JS',
-                level: [1,2,3]
+                level: [1, 2, 3, 4]
             },
             {
                 name: 'PHP',
-                level: [1,2,3]
+                level: [1, 2, 3]
             },
             {
                 name: 'MySQL',
-                level: [1,2,3]
+                level: [1, 2, 3]
             },
             {
                 name: 'Security',
-                level: [1]
+                level: [1, 2]
             }
         ];
 
         $scope.portfolios = [
+            {
+                title: 'Keno',
+                subtitle: 'Tabcorp Digital',
+                url: 'https://keno.com.au/',
+                avatar: 'https://keno.com.au/favicon.ico',
+                thumb: 'https://api.thumbalizr.com/?url=keno.com.au&api_key=&width=75&quality=80&encoding=jpg',
+                content: 'Keno is a subsidiary of Tabcorp Group, fun and easy to play random number game played every 3 minutes.'
+            },
             {
                 title: 'Unikrn',
                 subtitle: 'Tabcorp Wagering Technology',
@@ -56,7 +64,7 @@ angular.module('aku.about', [])
                 url: 'https://m.luxbet.com',
                 avatar: 'https://m.luxbet.com/images/luxbet-touch-icon-57x57-precomposed.png',
                 thumb: 'https://api.thumbalizr.com/?url=m.luxbet.com&api_key=&width=75&quality=80&encoding=jpg',
-                content: 'Luxbet is an AU-based betting website build using EmberJS Framework.'
+                content: 'Luxbet is an Australian-based betting website build using EmberJS Framework.'
             },
             {
                 title: 'Macworld Australia',
